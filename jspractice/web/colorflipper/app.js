3b8af5ce47721eddee8e098ca8895a8fe5f82373
simarjot <simarjot0032@gmail.com>
2023-08-01 15:15:30 +0530

    update

--- jspractice/web/colorflipper/app.js
+++ jspractice/web/colorflipper/app.js
@@ -5,4 +5,4 @@ document.querySelector('.click').addEventListener('click',function(){
  random=Math.floor(Math.random()*colors.length);
     background.style.background=colors[random];
     document.querySelector('.backh').innerHTML='BACKGROUND  COLOR : '+colors[random].toUpperCase();
-})
+});
