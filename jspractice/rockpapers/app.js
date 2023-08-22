3f4a5f169cd827c7a63330478a3fe6a838bdf455
simarjot <simarjot0032@gmail.com>
2023-08-22 23:24:04 +0530

    update

--- jspractice/rockpapers/app.js
+++ jspractice/rockpapers/app.js
@@ -5,9 +5,11 @@ let youoption=document.querySelector('.youo');
 let rock=document.querySelector('#rock');
 let paper=document.querySelector('#paper');
 let sissors=document.querySelector('#sissors');
-let r,p,s;
-let random;
-let closebtn=document.querySelector('.close')
+let r,p,s,random;
+let closebtn=document.querySelector('.close');
+let tick=new Audio('../rockpapers/sound/tink.wav');
+let lose=new Audio('../rockpapers/sound/lose (1).wav');
+let wins=new Audio('../rockpapers/sound/win (1).wav');
 let computeroption=[
     {
         opti:'<i class="fa-solid fa-hand-back-fist fa-5x" id="rock" style="color: burlywood;"></i>',
@@ -28,21 +30,25 @@ rock.addEventListener('click',function(){
      random=Math.floor(Math.random()*computeroption.length);
      computer.innerHTML=computeroption[random].opti;
     r='rock';
+    tick.play();
     setTimeout(() => {
         
     
     if(r===computeroption[random].choice){
         console.log('tie');
+
         winner.classList.toggle('hide');
         document.querySelector('.results').innerHTML='tie'
     }
    else if(computeroption[random].choice==='sissors'){
         winner.classList.toggle('hide');
+        wins.play();
         document.querySelector('.results').innerHTML='you won'
     }
     else{
         winner.classList.toggle('hide');
-        document.querySelector('.results').innerHTML='you loose'
+        document.querySelector('.results').innerHTML='you loose';
+        lose.play();
     }
 }, 550);
    
@@ -52,6 +58,7 @@ paper.addEventListener('click',function(){
     random=Math.floor(Math.random()*computeroption.length);
     p='paper';
     computer.innerHTML=computeroption[random].opti;
+    tick.play();
    setTimeout(() => {
     
  
@@ -62,20 +69,23 @@ paper.addEventListener('click',function(){
         }
         else if(computeroption[random].choice==='rock'){
             winner.classList.toggle('hide');
-            document.querySelector('.results').innerHTML='you won'
+            document.querySelector('.results').innerHTML='you won';
+            wins.play();
         }
         else{
             winner.classList.toggle('hide');
-            document.querySelector('.results').innerHTML='you loose'
+            document.querySelector('.results').innerHTML='you loose';
+            lose.play();
         }
     }, 500);
   
-})
+});
 sissors.addEventListener('click',function(){
     youoption.innerHTML='<i class="fa-solid fa-hand-scissors fa-5x"  style="color: burlywood;"></i>'
     random=Math.floor(Math.random()*computeroption.length);
     s='sissors';
     computer.innerHTML=computeroption[random].opti;
+    tick.play();
     setTimeout(() => {
     if(s===computeroption[random].choice){
         console.log('tie');
@@ -85,14 +95,16 @@ sissors.addEventListener('click',function(){
     }   
      else if(computeroption[random].choice==='paper'){
         winner.classList.toggle('hide');
-        document.querySelector('.results').innerHTML='you won'
+        document.querySelector('.results').innerHTML='you won';
+        wins.play();
     }
     else{
         winner.classList.toggle('hide');
         document.querySelector('.results').innerHTML='you loose'
+        lose.play();
     }
 }, 500);
-})
+});
 
 
 
@@ -104,8 +116,10 @@ sissors.addEventListener('click',function(){
 button.addEventListener('click',function(){
     document.querySelector('.box').classList.toggle('show')
     button.classList.toggle('hide');
-})
+    tick.play();
+});
 
 closebtn.addEventListener('click',function() {
     winner.classList.toggle('hide');
-})
+    tick.play();
+});
